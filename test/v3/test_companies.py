8e3e5bf018e27e0d3505d57ad47984a9f8a81e2f
Jacob212 <jacobhale.hale@gmail.com>
2019-11-07 13:27:50 +0000

    forgot this one

--- test/v3/test_companies.py
+++ test/v3/test_companies.py
@@ -4,12 +4,12 @@ from fulltmdb import companies
 class CompaniesTest(unittest.TestCase):
     def test_get_details(self):
         result = companies.get_details(1)
-        self.assertTrue(hasattr(result, 'id'))
+        self.assertTrue('id' in result)
 
     def test_get_alternative_names(self):
         result = companies.get_alternative_names(1)
-        self.assertTrue(hasattr(result, 'id'))
+        self.assertTrue('id' in result)
 
     def test_get_images(self):
         result = companies.get_images(1)
-        self.assertTrue(hasattr(result, 'id'))
+        self.assertTrue('id' in result)
