d97e9ab79358769b531385c51fcd1e4dcfd1104f
Jacob212 <jacobhale.hale@gmail.com>
2019-11-07 13:53:45 +0000

    added new files to init

--- fulltmdb/v3/__init__.py
+++ fulltmdb/v3/__init__.py
@@ -7,6 +7,8 @@ from . import companies as Companies
 from . import configuration as Configuration
 from . import credits as Credits
 from . import discover as Discover
+from . import episode_groups as Episode_groups
+from . import episodes as Episodes
 from . import find as Find
 from . import genres as Genres
 from . import guests as Guests
@@ -17,5 +19,6 @@ from . import networks as Networks
 from . import people as People
 from . import reviews as Reviews
 from . import search as Search
+from . import seasons as Seasons
 from . import trending as Trending
 from . import tv as Tv
