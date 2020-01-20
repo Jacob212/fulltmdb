ffec80db80b72ecd8b95b60a98f7783a8ce7e0fb
Jacob212 <jacobhale.hale@gmail.com>
2020-01-20 13:16:15 +0000

    lint

--- fulltmdb/v3/keywords.py
+++ fulltmdb/v3/keywords.py
@@ -4,22 +4,24 @@ from ..base import _call
 All api requests under the keywords tab in https://developers.themoviedb.org/3/keywords
 '''
 
+
 def details(keyword_id):
     '''
     Gets name and id of keyword
 
-    required: keyword_id 
+    required: keyword_id
     optional:
     '''
 
     return _call('GET', f'https://api.themoviedb.org/3/keyword/{keyword_id}')
+
 
 def movies(keyword_id, **kwargs):
     '''
     Get the movies that belong to a keyword.
     We highly recommend using https://developers.themoviedb.org/3/discover/movie-discover instead of this method as it is much more flexible.
 
-    required: keyword_id 
+    required: keyword_id
     optional:   language, include_adult
     '''
 
