c4ba7d6c25813f8f1a636d70b56ee4febc28dfae
Jacob212 <jacobhale.hale@gmail.com>
2020-03-13 17:52:07 +0000

    version bump

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 
 setup(
     name='fulltmdb',
-    version='0.1.3',
+    version='0.1.4',
     author='Jacob Hale',
     author_email='jacobhale.hale@gmail.com',
     description=('A wrapper for The Movie Database API v3 and v4.'),
