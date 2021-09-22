f8ef2711ce24372d380afe301c9769689706b3b8
Jacob212 <jacobhale.hale@gmail.com>
2021-09-23 00:01:37 +0100

    Fix cache test

--- test/v3/test_cache.py
+++ test/v3/test_cache.py
@@ -1,15 +1,16 @@
 import unittest
-import time
-from fulltmdb import Setup, changes
+from fulltmdb import Setup, Changes
 
 Setup.set_cache(3600)
-
+Setup.clear_cache()
 
 class CacheTest(unittest.TestCase):
     def test_cache(self):
         Setup.just_json(False)
-        result = changes.movie()
-        self.assertTrue(result.from_cache is True)
-        result = changes.movie(disable_cache=True)
+        result = Changes.movie()
+        result = Changes.movie()
+        result2 = Changes.movie(disable_cache=True)
         Setup.just_json(True)
-        self.assertFalse(hasattr(result, 'from_cache'))
+        self.assertTrue(hasattr(result, 'from_cache'))
+        self.assertTrue(str(result.from_cache) == 'True')
+        self.assertFalse(hasattr(result2, 'from_cache'))
