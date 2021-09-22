83ac23f1ebdb92d7f3c1a06ba8c56005ab0f2313
Jacob212 <jacobhale.hale@gmail.com>
2021-09-22 18:31:27 +0100

    Revert "Update test_cache.py"
    This reverts commit f6893b8c04c8db3484e6823913c52961e5464115.

--- test/v3/test_cache.py
+++ test/v3/test_cache.py
@@ -1,13 +1,28 @@
 import unittest
+import time
 from fulltmdb import Setup, changes
 
 Setup.set_cache(3600)
 
+
 class CacheTest(unittest.TestCase):
     def test_cache(self):
-        Setup.just_json(False)
-        result = changes.movie()
-        self.assertTrue(result.from_cache is True)
-        result = changes.movie(disable_cache=True)
-        self.assertFalse(hasattr(result, 'from_cache'))
-        Setup.just_json(True)
+        changes.movie()
+        now = time.time()
+        for i in range(50):
+            changes.movie()
+        total = time.time()-now
+        now = time.time()
+        for i in range(50):
+            changes.movie(disable_cache=True)
+        total2 = time.time()-now
+        now = time.time()
+        for i in range(50):
+            changes.movie()
+        total3 = time.time()-now
+        now = time.time()
+        for i in range(50):
+            changes.movie(disable_cache=True)
+        total4 = time.time()-now
+        self.assertTrue(total2>total)
+        self.assertTrue(total4>total3)
